14e0443eb8551f8f0d6933efc6c040fbfdd628e7
mumu111111 <2646709035@qq.com>
2019-09-01 18:52:09 +0800

    '数据劫持方法'

---
+++ main.js
@@ -0,0 +1,23 @@
+//实现双向绑定
+
+// data数据劫持
+function Observer(data) {
+    for (var key in data) {
+        let val = data[key]  //必须用let 形成块级作用域  保证val是每一个
+        Object.defineProperty(data, key, {
+            enumerable: true,
+            get() {
+                return val
+            },
+            set(newVal) {
+                if (newVal == val) {
+                    return
+                }
+                val = newVal
+
+            }
+
+        })
+    }
+}
+
